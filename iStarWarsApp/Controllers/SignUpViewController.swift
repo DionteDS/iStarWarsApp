d5cf58344e1a8c032796837253ce7ded2b839f17
Dionte <Dionte_sb@yahoo.com>
2020-02-05 21:10:32 -0800

    Implemented the sign up view controller

---
+++ iStarWarsApp/Controllers/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  iStarWarsApp
+//
+//  Created by Dionte Silmon on 2/5/20.
+//  Copyright © 2020 Dionte Silmon. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
