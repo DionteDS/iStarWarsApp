807f643129467fe167b4ee74de0497635ed9e6bb
Dionte <Dionte_sb@yahoo.com>
2020-02-05 23:02:30 -0800

    User can now sign out of the app

--- iStarWarsApp/Controllers/HomePageViewController.swift
+++ iStarWarsApp/Controllers/HomePageViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class HomePageViewController: UIViewController {
 
@@ -15,5 +16,22 @@ class HomePageViewController: UIViewController {
 
         navigationItem.hidesBackButton = true
     }
+    
+    @IBAction func tappedLogout(_ sender: UIBarButtonItem) {
+        
+        // Sign out of app
+        do {
+            try Auth.auth().signOut()
+            
+            // If root view controller is not nil Go back to root view controller (Log in and sign up page)
+            guard (navigationController?.popToRootViewController(animated: true)) != nil else {
+                print("No view controller to pop off too.")
+                return
+            }
+        } catch {
+            print("Error signing out, \(error)")
+        }
+    }
+    
 
 }
